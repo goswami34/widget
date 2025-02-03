ad86ecff5cd9ad31731ea6b30e986480423ab0a6
fatin-webEfo <fatin@webefo.com>
2025-02-03 17:26:23 +0600

    update script source URL in squareCraft.js for external loading

--- squareCraft.js
+++ squareCraft.js
@@ -1,7 +1,7 @@
 (async function SquareCraft() {
   
     const script = document.createElement("script");
-    script.src = "./src/components/parent.js"; 
+    script.src = "https://fatin-webefo.github.io/squareCraft-Plugin/src/components/parent.js"; 
     script.type = "module"; 
     script.onload = () => {
       console.log("🚀 Parent Script Loaded");
